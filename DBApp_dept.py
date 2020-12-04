f0012db12228c7d60d84ac448290cf51780ca48f
Bela7171 <54869350+Bela7171@users.noreply.github.com>
2020-12-04 21:05:16 +0100

    Snyggade till kod !

--- DBApp_dept.py
+++ DBApp_dept.py
@@ -9,27 +9,32 @@ mydb = mysql.connector.connect(
     passwd="pwd_" + group_number,
     database="ht20_2_project_group_" + group_number
 )
+table_align_format = "{0:30} {1}"
+table_align_format2 = "{0:35} {1:30} {2}"
 # Ask for department and make request from DB
 mycursor = mydb.cursor()
 dept_input = str(input("Department ID: "))
-mycursor.execute("SELECT name, dept_id FROM DEPARTMENTS WHERE dept_id LIKE" + "'%"+dept_input+"%'")
+mycursor.execute(f"SELECT name, dept_id FROM DEPARTMENTS WHERE dept_id LIKE '%{dept_input}%'") #+ "'%"+dept_input+"%'")
 result = mycursor.fetchall()
 result_length = len(result)
 
 if result_length > 1:
 # Path is a node - Lists all departments with same path
     print("These are the available departments: ")
-    print("Department\t path")
-    for x in result:
-        print(str(x[0])+"\t"+str(x[1]))
+    print(table_align_format.format("Department: ","path: "))
+    print("-----------------------------------------")
+    for name,path in result:
+        print(table_align_format.format(name,path))
+        #print(str(x[0])+"\t"+str(x[1]))
 else:
 # Path is a leaf - Lists all products of leafs
-    query = "SELECT P.name,P.dept_id, P.price FROM DEPARTMENTS AS D JOIN PRODUCTS AS P ON D.dept_id=P.dept_id WHERE P.dept_id = %s AND D.leaf = 1"
-    mycursor.execute(query, (dept_input,))
+    query = f"SELECT P.name,P.dept_id, P.price FROM DEPARTMENTS AS D JOIN PRODUCTS AS P ON D.dept_id=P.dept_id WHERE P.dept_id = '{dept_input}' AND D.leaf = 1"
+    mycursor.execute(query)
     products = mycursor.fetchall()
     print("These are the available products: ")
-    print("Name\t Department")
-    for x in products:
-        print(str(x[0])+"\t"+x[1])
+    print(table_align_format2.format("Name: ","Department: ", "Price: "))
+    print("-------------------------------------------------------------------------")
+    for name, path, price in products:
+        print(table_align_format2.format(name, path, price))
 ###################################
 mydb.close()
