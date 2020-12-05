cccfbf8333a76e3bb82870a8a79de8910936f49b
Bela7171 <54869350+Bela7171@users.noreply.github.com>
2020-12-05 02:48:10 +0100

    Uppdaterade DB med fler entries
    La in fler entries (departments och varor) för att underlätta komplettering av Task6
    
    Lekte även runt i Python och la till kontroller för inputs samt fixade format.

--- populate_departments.sql
+++ populate_departments.sql
@@ -1,46 +1,60 @@
+
 -- Special "homepage" - department
 INSERT INTO DEPARTMENTS(dept_id, parent_id, leaf, name, description)
 VALUES ("home/", NULL, 0, "Homepage", "Welcome to homepage");
 
 -- Toplevel - department:
--- EX:indexing = parent_id (home) & dept_id (electronics)  => 01 
-
 INSERT INTO DEPARTMENTS(dept_id, parent_id, leaf, name, description)
-VALUES 
-("home/electronics/", "home/", 0,"Electronics", "top level"),
-("home/apparel/", "home/", 0, "Apparel and accessories", "top level"),
-("home/books/", "home/", 0, "Books", "top level"),
-("home/health/", "home/", 0, "Health and Beauty", "top level"),
-("home/decoration/", "home/", 0, "Home decor and furnitures", "top level");
+VALUES
+("home/electronics/", "home/", 0,"Electronics", "Top level"),
+("home/apparel/", "home/", 0, "Apparel and accessories", "Top level"),
+("home/books/", "home/", 0, "Books", "Top level"),
+("home/health/", "home/", 0, "Health and Beauty", "Top level"),
+("home/decoration/", "home/", 0, "Home decor and furnitures", "Top level");
 
 
--- Child - DEPARTMENTS(dept_id, parent_id, name, description)parent_id (home/electronics) & dept_id (computer and tablets) => 011 
+-- Child - DEPARTMENTS
 
 -- Electronics:
 INSERT INTO DEPARTMENTS(dept_id, parent_id, leaf, name, description)
-VALUES 
-("home/electronics/computer/", "home/electronics/", 0, "Computer and tablets", "second level"),
-("home/electronics/console/", "home/electronics/", 0, "Video game and consoles", "second level");
+VALUES
+("home/electronics/computer/", "home/electronics/", 0, "Computer and tablets", "Second level"),
+("home/electronics/console/", "home/electronics/", 0, "Video game and consoles", "Second level"),
+("home/electronics/computer/desktop/", "home/electronics/computer/", 1, "Desktop", "Third level"), -- leaf
+("home/electronics/computer/laptop/", "home/electronics/computer/", 1, "Laptop", "Third level"), -- leaf
+("home/electronics/computer/tablet/", "home/electronics/computer/", 1, "Tablet", "Third level"); -- leaf
 
 
 -- Books:
 INSERT INTO DEPARTMENTS(dept_id, parent_id, leaf, name, description)
-VALUES 
-("home/books/auto-bio/", "home/books/", 1, "Autobiographies", "second level"), -- Leaf!
-("home/books/diy/", "home/books/", 1, "DIY", "second level"), -- Leaf!
-("home/books/course/", "home/books/", 0, "Course Litterature", "second level"), 
-("home/books/course/maths/", "home/books/course/", 1, "Mathmatics", "Third level");
+VALUES
+("home/books/course/", "home/books/", 0, "Course Litterature", "Second level"),
+("home/books/fiction/", "home/books/", 0, "Fiction", "Second level"),
+("home/books/auto-bio/", "home/books/", 1, "Autobiographies", "Second level"), -- Leaf!
+("home/books/diy/", "home/books/", 1, "DIY", "Second level"), -- Leaf!
+("home/books/course/maths/", "home/books/course/", 1, "Mathematics", "Third level"), -- Leaf!
+("home/books/course/biology/", "home/books/course/", 1, "Biology", "Third level"), -- Leaf!
+("home/books/course/chemistry/", "home/books/course/", 1, "Chemistry", "Third level"), -- Leaf!
+("home/books/course/physics/", "home/books/course/", 1, "Physics", "Third level"), -- Leaf!
+("home/books/course/it/", "home/books/course/", 1, "Information tech", "Third level"), -- Leaf!
+("home/books/fiction/crime/", "home/books/fiction/", 1, "Fiction-crime", "Third level"), -- Leaf!
+("home/books/fiction/historical/", "home/books/fiction/", 1, "Fiction-historical", "Third level"), -- Leaf!
+("home/books/fiction/fantasy/", "home/books/fiction/", 1, "Fiction-fantasy", "Third level"), -- Leaf!
+("home/books/fiction/romance/", "home/books/fiction/", 1, "Fiction-romance", "Third level"), -- Leaf!
+("home/books/fiction/horror/", "home/books/fiction/", 1, "Fiction-horror", "Third level"), -- Leaf!
+("home/books/fiction/scifi/", "home/books/fiction/", 1, "Fiction-science fiction", "Third level"); -- Leaf!
 
-
-
--- Child - DEPARTMENTS(dept_id, parent_id, name, description)parent_id (home/electronics/computer) & dept_id (computer and tablets) => 0111 
-
--- Electronics:
+-- Apparel and accessories
 INSERT INTO DEPARTMENTS(dept_id, parent_id, leaf, name, description)
-VALUES 
-("home/electronics/computer/desktop/", "home/electronics/computer/", 1, "Desktop", "third level"), -- leaf
-("home/electronics/computer/laptop/", "home/electronics/computer/", 1, "laptop", "third level"), -- leaf
-("home/electronics/computer/tablet/", "home/electronics/computer/", 1, "tablet", "third level"); -- leaf
-
-
+VALUES
+("home/apparel/women/", "home/apparel/", 0, "Women's apparel", "Second level"),
+("home/apparel/men/", "home/apparel/", 0, "Men's apparel", "Second level"),
+("home/apparel/child/", "home/apparel/", 1, "Children's apparel", "Second level"), -- Leaf!
+("home/apparel/accessories/", "home/apparel/", 1, "Accessories", "Second level"), -- Leaf!
+("home/apparel/women/dresses/", "home/apparel/women/", 1, "Women's apparel - dresses", "Third level"),-- Leaf!
+("home/apparel/women/training/", "home/apparel/women/", 1, "Women's apparel - training", "Third level"),-- Leaf!
+("home/apparel/women/indoors/", "home/apparel/women/", 1, "Women's apparel - indoors", "Third level"),-- Leaf!
+("home/apparel/men/suits/", "home/apparel/men/", 1, "Men's apparel - suits", "Third level"),-- Leaf!
+("home/apparel/men/training/", "home/apparel/men/", 1, "Men's apparel - training", "Third level"),-- Leaf!
+("home/apparel/men/indoors/", "home/apparel/men/", 1, "Men's apparel - indoors", "Third level");-- Leaf!
 
