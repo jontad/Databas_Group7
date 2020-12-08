1b86dbd57126528504f4fb971bd5d4738c8fd700
Fabian Haglund <till.fabian@gmail.com>
2020-12-08 13:09:21 +0100

    update DBApp_product with new model

--- DBApp_product.py
+++ DBApp_product.py
@@ -10,7 +10,7 @@ mydb = mysql.connector.connect(
     database="ht20_2_project_group_" + group_number
 )
 
-table_align_format = "{0:20} {1:20} {2:10} {3:10}"
+table_align_format = "{0:20} {1:20} {2:10}"
 result =[]
 
 mycursor = mydb.cursor()
@@ -24,17 +24,17 @@ while not result:
             print("That's not a valid option!")
 
     product_id = str(product_id)
-    get_products = """SELECT name, keyword, price, discount FROM PRODUCTS WHERE product_id = """ + product_id
+    get_products = """SELECT name, price, discount FROM PRODUCTS WHERE product_id = """ + product_id
     mycursor.execute(get_products)
     result = mycursor.fetchall()
     if not result:
         print("Item does not exist!")
 
 print("This is the sought product: ")
-print(table_align_format.format("Product: ", "Keyword: ", "Price: ","Discount: "))
+print(table_align_format.format("Product: ", "Price: ","Discount: "))
 print("------------------------------------------------------------------------")
-for name, keyword, price, discount in result:
-    print(table_align_format.format(name, keyword, price, discount))
+for name, price, discount in result:
+    print(table_align_format.format(name, price, discount))
 
 # Possibility to change discount - if yes, update DB
 answer = input("Would you like to change the discount? [y/n]: ")
@@ -46,10 +46,10 @@ if answer.lower() == 'y':
     result = mycursor.fetchall()
 
     print("Discount has been updated!:  ")
-    print(table_align_format.format("Product: ", "Keyword: ", "Price: ", "Discount: "))
+    print(table_align_format.format("Product: ", "Price: ", "Discount: "))
     print("-------------------------------------------------------------------------")
-    for name, keyword, price, discount in result:
-        print(table_align_format.format(name, keyword, price, discount))
+    for name, price, discount in result:
+        print(table_align_format.format(name, price, discount))
     mydb.commit()
 else:
     print("No change has been made!")
